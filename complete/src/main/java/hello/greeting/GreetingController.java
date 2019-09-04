5d1431c9df1a9f0c56068a06a82a207876ad3e1d
initiuminc1 <initiuminc1@gmail.com>
2019-09-04 17:31:49 +0530

    base configuration

--- complete/src/main/java/hello/greeting/GreetingController.java
+++ complete/src/main/java/hello/greeting/GreetingController.java
@@ -1,5 +1,7 @@
 package hello.greeting;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,19 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
+    private static final Logger LOG = LogManager.getLogger(GreetingController.class);
+
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        LOG.debug("Debugging log in our greeting method");
+        LOG.info("Info log in our greeting method");
+        LOG.warn("Warning log in our greeting method");
+        LOG.error("Error in our greeting method");
+        LOG.fatal("Damn! Fatal error. Please fix me.");
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
